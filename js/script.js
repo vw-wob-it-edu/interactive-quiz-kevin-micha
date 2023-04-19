e397d2e9764f320294a7cd090585c82d266d4740
kevin <kevin.kretz@volkswagen.de>
2023-04-19 11:29:20 +0200

    added .js questions and updated quiz

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const questions = [
+{
+  question: "Wie viele Sitzplätze hat der ID. 7 ?"
+  answers: [
+      { text: "4 Sitzplätze", correct: false},
+      { text: "5 Sitzplätze", correct: false},
+      { text: "6 Sitzplätze", correct: false},
+      { text: "7 Sitzplätze", correct: true},
+
+  ]
+}, {
+  question: "Wie groß ist die Batteriekapazität des ID. 7?"
+  answers: [
+    { text: "55 kWh", correct: false},
+    { text: "77 kWh", correct: true},
+    { text: "92 kWh", correct: false},
+    { text: "125 Kwh", correct: false},
+  ]
+}, {
+  question: "Welche Ladeleistung unterstützt der ID. 7 bei einer DC-Schnellladesäule?"
+  answers: [
+    { text: "95 kW", correct: false},
+    { text: "150 kW", correct: false},
+    { text: "170 kW", correct: true},
+    { text: "200 Kw", correct: false},
+}, {
+  question: "Wie hoch ist die maximale Leistung des VW ID.7?"
+  answers: [
+    { text: "150 PS", correct: false},
+    { text: "200 PS", correct: false},
+    { text: "250 PS", correct: true},
+    { text: "300 PS", correct: false},
+}, {
+  question: "Wie hoch ist die maximale Leistung des VW ID.7?"
+  answers: [
+    { text: "150 PS", correct: false},
+    { text: "200 PS", correct: false},
+    { text: "250 PS", correct: true},
+    { text: "300 PS", correct: false},
+}, {
+  question: "Wie viel Kofferraum Volumen bietet der VW ID. 7 ?"
+  answers: [
+    { text: "800 Liter (c.a. Tuareg)", correct: false},
+    { text: "650 Liter (ca. Passat B8)", correct: true},
+    { text: "500 Liter (ca. Tiguan)", correct: false},
+    { text: "380 Liter (ca. Golf 8)", correct: false},
+}, {
+  question: "Welche Assistenzsysteme sind im ID. 7 verfügbar ?"
+  answers: [
+    { text: "ACC", correct: false},
+    { text: "Spurhalteassistent", correct: false},
+    { text: "Notbremsassistent", correct: false},
+    { text: "alle der oben genannten", correct: true},
+}, {
+  question: "Wie ist die maximale Reichweite mit der Pro S Version (WLTP)?"
+  answers: [
+    { text: "500 Kilometer", correct: false},
+    { text: "600 Kilometer", correct: false},
+    { text: "700 Kilomete", correct: true},
+    { text: "800 Kilometer", correct: false},
+}, {
+  question: "Wie lange dauert es, von 10 auf 80 % zu Laden mit 200kW DC?"
+  answers: [
+    { text: "c.a. 20 Minuten", correct: true},
+    { text: "c.a. 30 Minuten", correct: false},
+    { text: "c.a. 45 Minuten", correct: false},
+    { text: "c.a. 45 - 60 Minuten", correct: false},
+}
+
+{
+  question: "Was findest du am besten beim ID. 7 ?"
+  answers: [
+    { text: "Das neue Elektrochromdach ", correct: true},
+    { text: "700 KM Reichweite mit fast 30min laden ", correct: true},
+    { text: "Das Limousinen Design als Elektro", correct: true},
+    { text: "Die neuen klimatisierten Sitze im ID.7", correct: true},
+
+
+
+]
